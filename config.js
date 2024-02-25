1e31cf4675f3f0700d6915792bbbb603d14cbaef
talossa <37369344+talossa@users.noreply.github.com>
2024-02-25 14:40:25 +0200

    tmdb key

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "https://unni-mw-proxy.netlify.app",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "a7edaeb0dc7cd49e54b193ecb9b17068",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhN2VkYWViMGRjN2NkNDllNTRiMTkzZWNiOWIxNzA2OCIsInN1YiI6IjY1ZGIyNzhlNjJmMzM1MDE2MzRjODE1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7EEF9ncrDqn0t87D3wQgpgzd7LrMlzQz_ENxTcVGnhw",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
